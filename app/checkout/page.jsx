3d3e103ca572d9e60b2eef256dbe5f345634bf49
Abdelrhman <justmaster.x1@gmail.com>
2025-07-30 07:48:44 +0300

    fixed useSearchParams problem

--- app/checkout/page.jsx
+++ app/checkout/page.jsx
@@ -1,27 +1,34 @@
 'use client'
-import React from 'react';
+import React, { Suspense } from 'react';
 import {Elements} from '@stripe/react-stripe-js';
 import {loadStripe} from '@stripe/stripe-js';
 import CheckoutForm from './_components/CheckoutForm';
 import { useSearchParams } from 'next/navigation';
 
-// Make sure to call `loadStripe` outside of a component’s render to avoid
+// Make sure to call `loadStripe` outside of a component's render to avoid
 // recreating the `Stripe` object on every render.
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHER_KEY);
 
-export default function App() {
+function CheckoutContent() {
   const searchParams = useSearchParams()
   const options = {
     mode:'payment',
     currency:'usd',
     amount:Number(searchParams.get('amount'))*100
   };
-console.log('hello');
+  console.log('hello');
 
   return (
     <Elements stripe={stripePromise} options={options}>
       <CheckoutForm  amount={Number(searchParams.get('amount'))}/>
-
     </Elements>
+  );
+}
+
+export default function App() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <CheckoutContent />
+    </Suspense>
   );
 };
